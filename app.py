2f5692d68fb7ea782f1ea9ac466a7260f69bed9e
A-La-Deen <billgeed2557@gmail.com>
2026-05-22 15:41:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("##### Day 1: การจัดการข้อมูลพื้น
 
 if st.button("💰 ระบบคำนวณส่วนลดตามยอดซื้อ"):
     st.switch_page("pages/app1_discount_calc.py")
-elif st.button("Data Clean"):
+elif st.button("ALADEEN Clean"):
     st.switch_page("pages/clean_ALADEEN.py")
 elif st.button("App Clean"):
     st.switch_page("pages/clean_app.py")
